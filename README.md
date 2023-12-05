88a5fc522a06c0b0f969ed8d10a0bc900815427d
jackherizsmith <jack@sourcerie.co>
2023-12-05 19:04:32 +0000

    day 3

--- README.md
+++ README.md
@@ -12,8 +12,8 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -76,16 +76,16 @@ Both parts: 2.77ms
 
 ```
 Day 02
-Time part 1: 0.318ms
-Time part 2: 0.836ms
-Both parts: 1.154ms
+Time part 1: -
+Time part 2: -
+Both parts: -
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 9.115ms
+Time part 2: 9.011ms
+Both parts: 18.126ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 2.77ms
+Total stars: 4/50
+Total time: 20.896ms
 ```
 
 <!--/RESULTS-->
--- src/day03/index.ts
+++ src/day03/index.ts
@@ -2,24 +2,99 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput;
 
-function isNumeric(n: string) {
+function isSymbol(n: string) {
+  return n !== "." && isNaN(parseFloat(n));
+}
+
+function isNumber(n: string) {
   return !isNaN(parseFloat(n)) && isFinite(+n);
+}
+
+type NumberData = {
+  value: number;
+  startX: number;
+  startY: number;
+  length: number;
+};
+
+type SymbolPosition = {
+  character: string;
+  x: number;
+  y: number;
+};
+
+function getSymbolPositions(rows: string[]) {
+  return rows
+    .flatMap((row, y) => {
+      const rowCharacters = row.split("");
+      return rowCharacters.map((character, x) => {
+        if (isSymbol(character)) {
+          return { character, x, y };
+        }
+      });
+    })
+    .filter(Boolean) as SymbolPosition[];
+}
+
+function getNumberPositions(data: NumberData[], row: string, y: number) {
+  let rowCharacters = row.split("");
+  let currentNumber = "";
+  rowCharacters.forEach((character, x, self) => {
+    const characterIsNumber = isNumber(character);
+    const isEndOfRow = self.length === x + 1;
+    const hasNumberToAdd = currentNumber.length > 0;
+    if (characterIsNumber) {
+      currentNumber += character;
+    }
+    if ((!characterIsNumber || isEndOfRow) && hasNumberToAdd) {
+      data.push({
+        value: Number(currentNumber),
+        startX: x - currentNumber.length,
+        startY: y,
+        length: currentNumber.length,
+      });
+      currentNumber = "";
+    }
+  });
+  return data;
+}
+
+function isAdjacent(symbolX: number, symbolY: number, numberX: number, numberY: number, numberLength: number) {
+  return (
+    symbolX >= numberX - 1 && symbolX <= numberX + numberLength && symbolY >= numberY - 1 && symbolY <= numberY + 1
+  );
 }
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput).split("\n");
-  const symbolsMap = input.reduce((symbols, row) => {
-    const rowCharaters = row.split("");
-    console.log(rowCharaters.map(isNumeric));
-    return symbols;
-  }, []);
-  return;
+
+  const symbolPositions = getSymbolPositions(input);
+  const numberData = input.reduce(getNumberPositions, []);
+
+  const neighbouredNumbers = numberData.filter(({ startX, startY, length }) => {
+    return symbolPositions.some(({ x, y }) => {
+      return isAdjacent(x, y, startX, startY, length);
+    });
+  });
+  return neighbouredNumbers.reduce((a, b) => a + b.value, 0);
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const input = parseInput(rawInput).split("\n");
 
-  return;
+  const symbolPositions = getSymbolPositions(input).filter(({ character }) => character === "*");
+  const numberData = input.reduce(getNumberPositions, []);
+
+  const relevantNumbers = symbolPositions.reduce((numbers: number[][], { x, y }) => {
+    const numberNeighbours = numberData.filter(({ startX, startY, length }) => {
+      return isAdjacent(x, y, startX, startY, length);
+    });
+    if (numberNeighbours.length > 1) {
+      numbers.push(numberNeighbours.map(({ value }) => value));
+    }
+    return numbers;
+  }, []);
+  return relevantNumbers.reduce((a, b) => a + b.reduce((c, d) => c * d), 0);
 };
 
 run({
@@ -44,13 +119,23 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..`,
+        expected: 467835,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
