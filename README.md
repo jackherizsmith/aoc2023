b52af5f41905ed5ab7744bad254bc643ecee65ee
jackherizsmith <jack@sourcerie.co>
2023-12-07 12:11:02 +0000

    tidy day 7

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%86/yellow)](src/day07)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -111,9 +111,9 @@ Both parts: 7.913ms
 
 ```
 Day 07
-Time part 1: 6.126ms
-Time part 2: -
-Both parts: 6.126ms
+Time part 1: 6.241ms
+Time part 2: 19.766ms
+Both parts: 26.006ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 11/50
-Total time: 37.034ms
+Total stars: 12/50
+Total time: 56.915ms
 ```
 
 <!--/RESULTS-->
--- src/day07/index.ts
+++ src/day07/index.ts
@@ -3,15 +3,16 @@ import { partition } from "../utils/index.js";
 
 const parseInput = (rawInput: string) => rawInput;
 
-const CHARS_TO_NUMS_1: Record<string, string> = {
-  A: "E",
-  K: "D",
-  Q: "C",
-  J: "B",
-  T: "A",
+type Rules = {
+  FIVE: (hand: HandData) => boolean;
+  FOUR: (hand: HandData) => boolean;
+  FULL: (hand: HandData) => boolean;
+  THREE: (hand: HandData) => boolean;
+  TWOPAIR: (hand: HandData) => boolean;
+  PAIR: (hand: HandData) => boolean;
 };
 
-const RULES_1 = {
+const RULES_1: Rules = {
   FIVE: (hand: HandData) => Object.values(hand).includes(5),
   FOUR: (hand: HandData) => Object.values(hand).includes(4),
   FULL: (hand: HandData) => Object.values(hand).includes(3) && Object.values(hand).includes(2),
@@ -20,42 +21,81 @@ const RULES_1 = {
   PAIR: (hand: HandData) => Object.values(hand).includes(2),
 };
 
+const hasXCards = (card: number | string, wilds: number, x: number) => typeof card === "number" && wilds + card === x;
+const hasHand = (hand: HandData, firstCount: number, secondCount?: number) => {
+  const wildcards = Number(hand["1"] || 0);
+  const remainingCards = { ...hand, "1": 0 };
+
+  if (!secondCount) {
+    return Object.values(remainingCards).some((card) => hasXCards(card, wildcards, firstCount));
+  }
+
+  const first = Object.entries(remainingCards).find(([, card]) => hasXCards(card, wildcards, firstCount));
+  if (!first) {
+    return false;
+  }
+  const remainingWild = firstCount - +first[1] - wildcards;
+  const handWithoutFirstCard = { ...remainingCards, [first[0]]: 0 };
+  return Object.values(handWithoutFirstCard).some((card) => hasXCards(card, remainingWild, secondCount));
+};
+
+const RULES_2: Rules = {
+  FIVE: (hand: HandData) => hasHand(hand, 5),
+  FOUR: (hand: HandData) => hasHand(hand, 4),
+  FULL: (hand: HandData) => hasHand(hand, 3, 2),
+  THREE: (hand: HandData) => hasHand(hand, 3),
+  TWOPAIR: (hand: HandData) => hasHand(hand, 2, 2),
+  PAIR: (hand: HandData) => hasHand(hand, 2),
+};
+
 type HandData = {
   value: string;
   bid: string;
   [key: string]: string | number;
 };
 
-const BY_VALUE = (a: HandData, b: HandData) => parseInt(b.value, 15) - parseInt(a.value, 15);
+const getHandsData = (hand: string) => {
+  const [value, bid] = hand.split(" ");
+  const cardCounts = value
+    .split("")
+    .reduce((counts: Record<string, number>, card) => ({ ...counts, [card]: (counts[card] || 0) + 1 }), {});
+  return { value, bid, ...cardCounts };
+};
+
+const byValue = (a: HandData, b: HandData) => parseInt(b.value, 15) - parseInt(a.value, 15);
+
+const getRankedHands = (handsData: HandData[], rulesObj: Rules) => {
+  const [fives, notFives] = partition(handsData, rulesObj.FIVE);
+  const [fours, notFours] = partition(notFives, rulesObj.FOUR);
+  const [full, notFull] = partition(notFours, rulesObj.FULL);
+  const [threes, notThrees] = partition(notFull, rulesObj.THREE);
+  const [twoPairs, notTwoPairs] = partition(notThrees, rulesObj.TWOPAIR);
+  const [pairs, notPairs] = partition(notTwoPairs, rulesObj.PAIR);
+  return [
+    ...fives.sort(byValue),
+    ...fours.sort(byValue),
+    ...full.sort(byValue),
+    ...threes.sort(byValue),
+    ...twoPairs.sort(byValue),
+    ...pairs.sort(byValue),
+    ...notPairs.sort(byValue),
+  ].reverse();
+};
+
+const CHARS_TO_NUMS_1: Record<string, string> = {
+  A: "E",
+  K: "D",
+  Q: "C",
+  J: "B",
+  T: "A",
+};
 
 const part1 = (rawInput: string): number => {
   const input = parseInput(rawInput)
     .replace(/[AKQJT]/g, (m) => CHARS_TO_NUMS_1[m])
     .split("\n");
-
-  const handsData = input.map((hand) => {
-    const [value, bid] = hand.split(" ");
-    const cardCounts = value
-      .split("")
-      .reduce((counts: Record<string, number>, card) => ({ ...counts, [card]: (counts[card] || 0) + 1 }), {});
-    return { value, bid, ...cardCounts };
-  });
-
-  const [fives, notFives] = partition(handsData, RULES_1.FIVE);
-  const [fours, notFours] = partition(notFives, RULES_1.FOUR);
-  const [full, notFull] = partition(notFours, RULES_1.FULL);
-  const [threes, notThrees] = partition(notFull, RULES_1.THREE);
-  const [twoPairs, notTwoPairs] = partition(notThrees, RULES_1.TWOPAIR);
-  const [pairs, notPairs] = partition(notTwoPairs, RULES_1.PAIR);
-  const rankedHands = [
-    ...fives.sort(BY_VALUE),
-    ...fours.sort(BY_VALUE),
-    ...full.sort(BY_VALUE),
-    ...threes.sort(BY_VALUE),
-    ...twoPairs.sort(BY_VALUE),
-    ...pairs.sort(BY_VALUE),
-    ...notPairs.sort(BY_VALUE),
-  ].reverse();
+  const handsData = input.map(getHandsData);
+  const rankedHands = getRankedHands(handsData, RULES_1);
   return rankedHands.reduce((a, b, i) => a + +b.bid * (i + 1), 0);
 };
 
@@ -67,77 +107,12 @@ const CHARS_TO_NUMS_2: Record<string, string> = {
   T: "A",
 };
 
-const splitHand = (hand: HandData) => ({ wildcards: Number(hand["1"] || 0), remainingCards: { ...hand, "1": 0 } });
-const hasXCards = (card: number | string, wilds: number, x: number) => typeof card === "number" && wilds + card === x;
-
-const RULES_2 = {
-  FIVE: (hand: HandData) => {
-    const { wildcards, remainingCards } = splitHand(hand);
-    return Object.values(remainingCards).some((card) => hasXCards(card, wildcards, 5));
-  },
-  FOUR: (hand: HandData) => {
-    const { wildcards, remainingCards } = splitHand(hand);
-    return Object.values(remainingCards).some((card) => hasXCards(card, wildcards, 4));
-  },
-  FULL: (hand: HandData) => {
-    const { wildcards, remainingCards } = splitHand(hand);
-    const three = Object.entries(remainingCards).find(([, card]) => hasXCards(card, wildcards, 3));
-    if (!three) {
-      return false;
-    }
-    const remainingWild = 3 - +three[1] - wildcards;
-    const handWithoutThreeCard = { ...hand, [three[0]]: 0 };
-    return Object.values(handWithoutThreeCard).some((card) => hasXCards(card, remainingWild, 2));
-  },
-  THREE: (hand: HandData) => {
-    const { wildcards, remainingCards } = splitHand(hand);
-    return Object.values(remainingCards).some((card) => hasXCards(card, wildcards, 3));
-  },
-  TWOPAIR: (hand: HandData) => {
-    const { wildcards, remainingCards } = splitHand(hand);
-    const two = Object.entries(remainingCards).find(([, card]) => hasXCards(card, wildcards, 2));
-    if (!two) {
-      return false;
-    }
-    const remainingWild = 2 - +two[1] - wildcards;
-    const handWithoutTwoCard = { ...hand, [two[0]]: 0 };
-    return Object.values(handWithoutTwoCard).some((card) => hasXCards(card, remainingWild, 2));
-  },
-  PAIR: (hand: HandData) => {
-    const { wildcards, remainingCards } = splitHand(hand);
-    return Object.values(remainingCards).some((card) => hasXCards(card, wildcards, 2));
-  },
-};
-
 const part2 = (rawInput: string): number => {
   const input = parseInput(rawInput)
     .replace(/[AKQJT]/g, (m) => CHARS_TO_NUMS_2[m])
     .split("\n");
-
-  const handsData = input.map((hand) => {
-    const [value, bid] = hand.split(" ");
-    const cardCounts = value
-      .split("")
-      .reduce((counts: Record<string, number>, card) => ({ ...counts, [card]: (counts[card] || 0) + 1 }), {});
-    return { value, bid, ...cardCounts };
-  });
-
-  const [fives, notFives] = partition(handsData, RULES_2.FIVE);
-  const [fours, notFours] = partition(notFives, RULES_2.FOUR);
-  const [full, notFull] = partition(notFours, RULES_2.FULL);
-  const [threes, notThrees] = partition(notFull, RULES_2.THREE);
-  const [twoPairs, notTwoPairs] = partition(notThrees, RULES_2.TWOPAIR);
-  const [pairs, notPairs] = partition(notTwoPairs, RULES_2.PAIR);
-  const rankedHands = [
-    ...fives.sort(BY_VALUE),
-    ...fours.sort(BY_VALUE),
-    ...full.sort(BY_VALUE),
-    ...threes.sort(BY_VALUE),
-    ...twoPairs.sort(BY_VALUE),
-    ...pairs.sort(BY_VALUE),
-    ...notPairs.sort(BY_VALUE),
-  ].reverse();
-  console.log(rankedHands.map((a) => a.bid));
+  const handsData = input.map(getHandsData);
+  const rankedHands = getRankedHands(handsData, RULES_2);
   return rankedHands.reduce((a, b, i) => a + +b.bid * (i + 1), 0);
 };
 
@@ -159,13 +134,11 @@ QQQJA 483`,
     tests: [
       {
         input: `
-AAAAJ 5
-AAAKJ 4
-AAAKK 32
-AJJKQ 3
-AAKKQ 22
-AJKQT 2
-AKQT9 1
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
         `,
         expected: 5905,
       },
